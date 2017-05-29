9e63e003b80f57286529ffbc42719619d461a0a6
mateusz <mateuszalmannai@googlemail.com>
2017-05-29 21:06:00 +0200

    move data from controller to model-hook in route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,13 +1,6 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  foods: [
-    {name: 'Tacos', isAvailable: true},
-    {name: 'Salad', isAvailable: true},
-    {name: 'Pizza', isAvailable: false},
-    {name: 'Vegetables', isAvailable: true},
-    {name: 'Fruit Smoothies', isAvailable: false}
-  ],
   restaurant: {name: 'Our Awesome Restaurant', yearsOpen: 1},
   actions: {
     makeUnavailable(food){
