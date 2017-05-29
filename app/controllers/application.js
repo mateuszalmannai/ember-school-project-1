7c5e6d789f9b6557b4b55856d5f40cecfc8a02ca
mateusz <mateuszalmannai@googlemail.com>
2017-05-29 18:07:17 +0200

    add array of objects in controller and access properties from template

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,12 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Controller.extend({
-  foods: ['Tacos', 'Salad', 'Pizza', 'Vegetables', 'Fruit Smoothies']
+  foods: [
+    {name: 'Tacos', isAvailable: true},
+    {name: 'Salad', isAvailable: true},
+    {name: 'Pizza', isAvailable: false},
+    {name: 'Vegetables', isAvailable: true},
+    {name: 'Fruit Smoothies', isAvailable: false}
+  ],
+  restaurant: {name: 'Our Awesome Restaurant', yearsOpen: 1}
 });
