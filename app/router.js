a98f2612be50236c444d97e0834734df08d817ab
mateusz <mateuszalmannai@googlemail.com>
2017-05-31 20:39:21 +0200

    add templates for implicit index routes and edit route

--- app/router.js
+++ app/router.js
@@ -12,6 +12,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('foods', function(){
     this.route('food', {path: ':food_id'});
+    this.route('food.edit', {path: ':food_id/edit'});
   });
   this.route('about');
   this.route('contact');
