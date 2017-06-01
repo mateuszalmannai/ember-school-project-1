d8ab6829ffe3239cff3a7d2bb52459151615400c
mateusz <mateuszalmannai@googlemail.com>
2017-06-01 19:38:01 +0200

    toggle properties to open and close items

--- app/components/food-item.js
+++ app/components/food-item.js
@@ -1,14 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isOpen: false,
   actions: {
-    makeUnavailable(food){
-      Ember.set(food, 'isAvailable', false);
+    toggleAvailablity(food){
+      //Ember.set(food, 'isAvailable', !food.get('isAvailable'));
+      food.set('isAvailable', !food.get('isAvailable'));  // Does the same thing, as long as food is an Ember object
+      // food.toggleProperty('isAvailable');
       food.save();
     },
-    makeAvailable(food){
-      Ember.set(food, 'isAvailable', true);
-      food.save();
+    toggleOpen(){
+      // 'this' is the context of the component
+      // this.set('isOpen', !this.get('isOpen'));
+      this.toggleProperty('isOpen'); // use ember method instead to achieve the same thing more succinctly
     }
   }
 });
