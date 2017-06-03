addfc9b6ef18aa2ebd6636f45b81a838fc6a534d
mateusz <mateuszalmannai@googlemail.com>
2017-06-03 18:22:14 +0200

    persist relationship

--- app/controllers/foods/food/edit.js
+++ app/controllers/foods/food/edit.js
@@ -5,6 +5,14 @@ export default Ember.Controller.extend({
     save(){
       this.get('model.food').save();
       this.transitionToRoute('foods.food', this.get('model.food.id'));
+    },
+    selectRestaurant(selection, component){
+      let food = this.get('model.food');
+      food.get('restaurant').then((restaurant)=>{
+        food.set('restaurant', selection); // 1. save food
+        selection.save();                  // 2. save new restaurant
+        restaurant.save();                 // 3. save old restaurant
+      });
     }
   }
 });
