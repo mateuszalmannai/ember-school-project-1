e33a2b6b1cd2589ba9a78a45330109d84a087e68
mateusz <mateuszalmannai@googlemail.com>
2017-06-03 12:21:12 +0200

    add belongsTo relatinship to food and display on show page

--- app/models/food.js
+++ app/models/food.js
@@ -4,5 +4,6 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   isAvailable: DS.attr('boolean'),
   description: DS.attr('string'),
-  imageUrl: DS.attr('string')
+  imageUrl: DS.attr('string'),
+  restaurant: DS.belongsTo('restaurant') // parameter optional if model name matches the property it is assigned to
 });
