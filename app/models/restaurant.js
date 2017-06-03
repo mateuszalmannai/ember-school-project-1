a240ba6f526566f55c240c863ea4e2d47f8dfe78
mateusz <mateuszalmannai@googlemail.com>
2017-06-03 12:09:40 +0200

    add hasMany relationship to restaurant model and loop through foods to display their name

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  foods: DS.hasMany('food') // name of model is optional, if name of attribute is plural of model
 });
