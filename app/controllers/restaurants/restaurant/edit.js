0c00ff9478212905308874e4a34caa25e91ca34f
mateusz <mateuszalmannai@googlemail.com>
2017-06-03 20:23:03 +0200

    provide selection of foods per restaurant

--- app/controllers/restaurants/restaurant/edit.js
+++ app/controllers/restaurants/restaurant/edit.js
@@ -1,10 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // take the foods in the model and subtract away the foods that are at the restaurant already
+  otherFoods: Ember.computed.setDiff('model.foods', 'model.restaurant.foods'),
   actions: {
     save(){
-      this.get('model').save();
-      this.transitionToRoute('restaurants.restaurant', this.get('model.id'));
+      this.get('model.restaurant').save();
+      this.transitionToRoute('restaurants.restaurant', this.get('model.restaurant.id'));
+    },
+    removeFood(food){
+      // specific to firebase
+      let restaurant = this.get('model.restaurant');
+      restaurant.get('foods').removeObject(food);
+      // specific to firebase: the food which belongs to a restaurant gets saved first
+      food.save().then(()=>{
+        // then, once the promise returns, the restaurant is saved
+        restaurant.save();
+      });
+    },
+    addFood(food){
+      // specific to firebase
+      let restaurant = this.get('model.restaurant');
+      restaurant.get('foods').addObject(food);
+      // specific to firebase: the food which belongs to a restaurant gets saved first
+      food.save().then(()=>{
+        // then, once the promise returns, the restaurant is saved
+        restaurant.save();
+      });
     }
   }
 });
