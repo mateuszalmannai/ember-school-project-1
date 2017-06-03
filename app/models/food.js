bce15ec137dd4bb992be9fff544337205a90db7a
mateusz <mateuszalmannai@googlemail.com>
2017-06-03 16:29:02 +0200

    get x-select to work

--- app/models/food.js
+++ app/models/food.js
@@ -5,5 +5,6 @@ export default DS.Model.extend({
   isAvailable: DS.attr('boolean'),
   description: DS.attr('string'),
   imageUrl: DS.attr('string'),
-  restaurant: DS.belongsTo('restaurant') // parameter optional if model name matches the property it is assigned to
+  restaurant: DS.belongsTo('restaurant'), // parameter optional if model name matches the property it is assigned to
+  course: DS.attr('string')
 });
